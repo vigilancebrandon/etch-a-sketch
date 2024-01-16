63c8f4a41e65c37a02937f1f05d42dfdace4c485
Vigilance <152765003+vigilancebrandon@users.noreply.github.com>
2024-01-16 13:28:01 -0500

    Split up makeGrid function into sub funcs
    Changed makeGrid to makeNewGrid, and split up the function into
    makeRows and makeCols for better readability.

--- script.js
+++ script.js
@@ -14,23 +14,52 @@ sizeBtn.onclick = () => {
     let gridSize = prompt("Whoops, looks like your number was too big or too small!\n" +
     "Please input an integer 10 to 100 for grid size.");
   }
-  makeGrid();
+  makeGrid(gridSize);
 }
 
-function makeGrid(gridSize) {
+/* function clearGrid() {
+  container.removeChild
+}*/
+
+function makeRows(gridSize) {
   for (let i = 0; i < gridSize; i++) {
     let divRow = document.createElement("div");
     container.appendChild(divRow);
+    divRow.classList = "row";
+  }
+}
+
+function makeCols(gridSize) {
+  for (const divRow of document.querySelectorAll(".row")) {
+    let i = 0;
     for (let k = 0; k < gridSize; k++) {
       let divCol = document.createElement("div");
       divRow.appendChild(divCol);
       divCol.classList = "col";
       divCol.id = `${k}, ${i}`;
     }
+    i++;
+  }
+}
+
+function makeGrid(gridSize) {
+  makeRows(gridSize);
+  makeCols(gridSize);
+}
+
+/*function makeGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    let divRow = document.createElement("div");
+    for (let k = 0; k < gridSize; k++) {
+      let divCol = document.createElement("div");
+      divRow.appendChild(divCol);
+      divCol.classList = "col";
+      divCol.id = `${k}, ${i}`;
+    }
+    container.appendChild(divRow);
     divRow.classList = "row";
-}}
+}}*/
 
 container.addEventListener("mouseover", (event) => {
   event.target.style.backgroundColor = 'black';
 })
-  
