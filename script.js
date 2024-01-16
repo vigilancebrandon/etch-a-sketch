6be71222b106e8a6eea0fd4bf323ac167656e2f5
Vigilance <152765003+vigilancebrandon@users.noreply.github.com>
2024-01-16 12:46:06 -0500

    Edit js
    Added event handler using event delegation to change background color
    of grid elements on hover. Also added general var gridSize for easily
    adjustable resolution.

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 let container = document.querySelector(".container")
 
-for (let i = 0; i < 16; i++) {
+let gridSize = 256
+
+for (let i = 0; i < gridSize; i++) {
   let divRow = document.createElement("div");
   container.appendChild(divRow);
-  for (let k = 0; k < 16; k++) {
+  for (let k = 0; k < gridSize; k++) {
     let divCol = document.createElement("div");
     divRow.appendChild(divCol);
     divCol.classList = "col";
-    divCol.id = `${k}, ${i}`
+    divCol.id = `${k}, ${i}`;
   }
   divRow.classList = "row";
-  divRow.id = `${i}`
+  //divRow.id = `${i}`;
 }
+
+//let divCol = document.querySelectorAll(".col");
+
+container.addEventListener("mouseover", (event) => {
+  event.target.style.backgroundColor = 'black';
+  //console.log(event.target);
+})
   
