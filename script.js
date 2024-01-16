7ec7819af747b078313fb0dd9e2c235c00cbc89a
Vigilance <152765003+vigilancebrandon@users.noreply.github.com>
2024-01-16 11:46:40 -0500

    Change subDivs to correct num, link stylesheet
    Changed for loop in js to make 256 divs for proper 16x16 grid. Also
    corrected HTML head to link stylesheet.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let container = document.querySelector(".container")
 
-for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= 256; i++) {
   let subDiv = document.createElement("div");
   container.appendChild(subDiv);
   subDiv.classList = "subDiv";
