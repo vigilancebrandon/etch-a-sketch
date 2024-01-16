4f54fb3168c119a463bea6fc1db60821a25ec8db
Vigilance <152765003+vigilancebrandon@users.noreply.github.com>
2024-01-16 12:26:23 -0500

    Edit js, create 16 row divs, each with 16 col divs

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let container = document.querySelector(".container")
 
-for (let i = 1; i <= 256; i++) {
-  let subDiv = document.createElement("div");
-  container.appendChild(subDiv);
-  subDiv.classList = "subDiv";
+for (let i = 1; i <= 16; i++) {
+  let divRow = document.createElement("div");
+  container.appendChild(divRow);
+  for (let i = 1; i <= 16; i++) {
+    let divCol = document.createElement("div");
+    divRow.appendChild(divCol);
+    divCol.classList = "col";
+  }
+  divRow.classList = "row";
 }
   
